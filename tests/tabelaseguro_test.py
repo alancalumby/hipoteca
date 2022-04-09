ff54aed67a63ee8cf2dd561469e085bb2d95756a
Alan Calumby <alancalumby@gmail.com>
2022-04-09 01:45:39 -0400

    pequenas melhorias

--- tests/tabelaseguro_test.py
+++ tests/tabelaseguro_test.py
@@ -4,7 +4,7 @@ import tabelaseguro
 class TestTabelaSeguro(unittest.TestCase):
 
     def test_init(self):
-        tabelaSeguro = tabelaseguro.TabelaSeguro(10,15,20,100,4.0,3.1,2.8,0)
+        tabelaSeguro = tabelaseguro.TabelaSeguro(10, 15, 20, 100, 4.0, 3.1, 2.8, 0)
         self.assertEqual(tabelaSeguro.percentual_nivel_1, 10)
         self.assertEqual(tabelaSeguro.percentual_nivel_2, 15)
         self.assertEqual(tabelaSeguro.percentual_nivel_3, 20)
@@ -15,9 +15,9 @@ class TestTabelaSeguro(unittest.TestCase):
         self.assertEqual(tabelaSeguro.aliquota_nivel_4, 0)
 
     def test_calcular_percentual_entrada(self):
-        tabelaSeguro = tabelaseguro.TabelaSeguro(10,15,20,100,4.0,3.1,2.8,0)
-        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(5.0), 4.0)
-        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(10.0), 3.1)
-        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(15.0), 2.8)
-        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(20.0), 0)
-        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(25.0), 0)
+        tabelaSeguro = tabelaseguro.TabelaSeguro(10, 15, 20, 100, 4.0, 3.1, 2.8, 0)
+        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(percentual_entrada = 5.0), 4.0)
+        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(percentual_entrada = 10.0), 3.1)
+        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(percentual_entrada = 15.0), 2.8)
+        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(percentual_entrada = 20.0), 0)
+        self.assertEqual(tabelaSeguro.calcular_percentual_seguro(percentual_entrada = 25.0), 0)
